72796a4fad008c2d94b5493bc295b3cd280704fa
Trum7 <cadajerp@unal.edu.co>
2016-08-20 18:02:15 -0500

    Contact us link

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'pages#index'
-
+  get 'contactUS', to:'pages#contactUS'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
