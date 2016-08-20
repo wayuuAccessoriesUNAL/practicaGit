8cd0c55016fd3a8aa5310631fbe025af405a6781
Daniel Navas <dfnavasm@unal.edu.co>
2016-08-20 18:28:23 -0500

    aboutUs page updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#index'
+  get 'aboutUs', to: 'pages#aboutUs'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
