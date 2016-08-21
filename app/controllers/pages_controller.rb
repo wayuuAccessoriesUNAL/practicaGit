8929ae836dd94ae178a4e9c39d8d65daf64a2f0f
Cristian Orjuela <ccorjuelav@unal.edu.co>
2016-08-20 19:50:37 -0500

    Add link to the about us controller.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,4 @@
 class PagesController < ApplicationController
-<<<<<<< HEAD
 
 	def index
 	end
@@ -7,15 +6,7 @@ class PagesController < ApplicationController
 	def aboutUs
 	end
 
-def test
-
-
-=======
-  def index
-  end
-
   def contactUS
   end
 
->>>>>>> 72796a4fad008c2d94b5493bc295b3cd280704fa
 end
