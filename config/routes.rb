5ad7f491008d680b284dde270433f652ca5e1e3f
Cristian Orjuela <ccorjuelav@unal.edu.co>
2016-08-20 20:23:02 -0500

    URLs and validations were added in this commit.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
+  resources :messages
   root to: 'pages#index'
-<<<<<<< HEAD
   get 'aboutUs', to: 'pages#aboutUs'
-
-=======
   get 'contactUS', to:'pages#contactUS'
->>>>>>> 72796a4fad008c2d94b5493bc295b3cd280704fa
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
