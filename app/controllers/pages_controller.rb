040a795e741e4119c918f6d11920e127c33fc524
Daniel Navas <dfnavasm@unal.edu.co>
2016-08-20 19:17:28 -0500

    merge aboutUs into master

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,11 +6,16 @@ class PagesController < ApplicationController
 
 	def aboutUs
 	end
+
+def test
+
+
 =======
   def index
   end
 
   def contactUS
   end
+
 >>>>>>> 72796a4fad008c2d94b5493bc295b3cd280704fa
 end
